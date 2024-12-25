51ed4ebb3204e9d3055281ac1f2d2759caf55f9c
a0905114287 <a0905114287@gmail.com>
2024-12-25 19:23:57 +0800

    1225

--- sever.js
+++ sever.js
@@ -52,6 +52,6 @@ server.post("/contact_me", (req,res)=>{
      res.redirect("/#contact");
 })
 
-server.listen(80, ()=>{
+server.listen(6969, ()=>{
     console.log("Server is running at port 80.");
 })
