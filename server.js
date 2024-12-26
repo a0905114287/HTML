4336659fdd3868d30cb61edb635c38710ce59533
a0905114287 <a0905114287@gmail.com>
2024-12-26 15:56:41 +0800

    1226

--- server.js
+++ server.js
@@ -2,67 +2,57 @@
 //install web server package: express >npm install express
 var express = require("express");
 var server = express();
+var bodyParser = require("body-parser");
 var path = require("path");
 
 //web root
-server.use(express.static(path.join(__dirname)));
-
+server.use(express.static(__dirname));
+server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
 
 var fileUpload = require("express-fileupload");
 server.use(fileUpload({defCharset:'utf8', defParamCharset:'utf8'}));
 
 
 var DB = require("nedb-promises");
-var ProfolioDB = DB.create(path.join(__dirname+"/profolio.db"));
-var ContactDB = DB.create(path.join(__dirname+"/contact.db"));
+var ProfolioDB = DB.create(__dirname+"/profolio.db");
+var ContactDB = DB.create(__dirname+"/contact.db");
  
-server.use(express.json());
-server.use(express.urlencoded({ extended: true }));
-/*ProfolioDB.insert([
+
+  /*ProfolioDB.insert([
     {modal:"card1",imgSrc:"picture/s1411122027材質.png",link:"3D_design.html",title:"3D design",text:"See More"},
     {modal:"card2",imgSrc:"picture/達悟族.png",link:"graphic_design.html",title:"graphic design",text:"See More"},
     {modal:"card3",imgSrc:"picture/S__9060369.png",link:"Something_else_here.html",title:"Something_else_here",text:"See More"}
 ])*/
 
-/*server.get("/services", (req, res)=>{
+server.get("/services", (req, res)=>{
     //DB find
     var Services=[
         {icon: "fa-shopping-cart", heading:"E-Commerce", text:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."},
         {icon: "fa-laptop", heading:"Responsive Design", text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima maxime quam architecto quo inventore harum ex magni, dicta impedit."}
     ];
     res.send(Services);
-});*/
+});
 
 server.get("/profolio", (req,res)=>{
       //DB
-      ProfolioDB.find({}).then(results => {
-        if (results.length > 0) {
-            res.send(results);
-        } else {
-            res.status(404).send("No records found!");
+      ProfolioDB.find({}).then(results=>{
+        if(results != null){
+             res.send(results);
+        }else{
+            res.send("Error!");
         }
-    })
-    .catch(err => {
-        console.error(err);
-        res.status(500).send("Database error!");
-    });
+      })
 })
 
-server.post("/contact_me", (req, res) => {
-    ContactDB.insert(req.body)
-      .then(() => {
-          res.redirect("/#contact");
-      })
-      .catch(err => {
-          console.error(err);
-          res.status(500).send("Failed to save contact information!");
-      });
-});
+server.post("/contact_me", (req,res)=>{
+     ContactDB.insert(req.body);
+     res.redirect("/#contact");
+})
 
 server.listen(2170, ()=>{
     console.log("Server is running at port 2170.");
 })
-
 server.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/main.html'));
   });
